f52aeb206ca2dcb8b10ee11d9f59fe209aad70f2
Vlad Zahiu <vladzahiu28@gmail.com>
2022-12-28 11:47:40 +0200

    Add ANSI commands.

---
+++ SharpConsole/Util/AnsiCommand.cs
@@ -0,0 +1,55 @@
+﻿namespace SharpConsole.Util;
+
+/// <summary>
+/// Provides definitions for the most common ANSI commands.
+/// </summary>
+/// <remarks>
+/// The ANSI command code resides at the end of an ANSI sequence.
+/// </remarks>
+public enum AnsiCommand
+{
+    /// <summary>
+    /// Move the cursor up a number of cells.
+    /// </summary>
+    CursorUp = 'A',
+    /// <summary>
+    /// Move the cursor down a number of cells.
+    /// </summary>
+    CursorDown = 'B',
+    /// <summary>
+    /// Move the cursor forwards a number of cells.
+    /// </summary>
+    CursorForward = 'C',
+    /// <summary>
+    /// Move the cursor backwards a number of cells.
+    /// </summary>
+    CursorBackward = 'D',
+    /// <summary>
+    /// Move the cursor a number of lines down, at the beginning of the line.
+    /// </summary>
+    CursorNextLine = 'E',
+    /// <summary>
+    /// Move the cursor a number of lines up, at the beginning of the line.
+    /// </summary>
+    CursorPreviousLine = 'F',
+    /// <summary>
+    /// Move the cursor to a specific position.
+    /// </summary>
+    CursorPosition = 'H',
+    /// <summary>
+    /// Clear part of the screen.
+    /// </summary>
+    ClearScreen = 'J',
+    /// <summary>
+    /// Scroll the whole page up by a number of lines.
+    /// </summary>
+    ScrollUp = 'S',
+    /// <summary>
+    /// Scroll the whole page down by a number of lines.
+    /// </summary>
+    ScrollDown = 'T',
+    /// <summary>
+    /// Set the colors and style of the following characters.
+    /// </summary>
+    SelectGraphicRendition = 'm'
+}
